9d66ff5b3b6a194869776d672a68f5affe4d6d43
subratkhilar <subrat.khilar@gmail.com>
2018-08-15 11:30:51 +0530

    firt commt

---
+++ js/main.js
@@ -0,0 +1,73 @@
+var username = "";
+function askUsername() {
+  send_message("Hello, What's Your name?");
+}
+ 
+function send_message(message) {
+  var prevMessage = $("#container").html();
+  if(prevMessage!=0)
+  prevMessage = prevMessage+"";
+ 
+  $("#container").html(prevMessage+ "<br><span class='current_message'><span class='bot'> Chatbot: </span>" + message+"</span>");
+  $(".current_message").hide();
+  $(".current_message").delay(400).fadeIn();
+  $(".current_message").removeClass("current_message");
+ 
+}
+ 
+function ai(message) {
+  if (username.length <3) {
+    username = message;
+    send_message("Welcome " + message+ ", What are you doing so?");
+  }
+ 
+  if(message.indexOf("how are you")>=0|| message.indexOf("and you")>=0)
+  {
+    send_message( "I am good");
+  }
+ 
+  if(message.indexOf("time")>=0|| message.indexOf("can you tell me time")>=0)
+  {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    send_message("Current Time is :  "+ h+":"+m );
+  }
+ 
+}
+ 
+$(function() {
+ 
+  askUsername();
+ 
+  $("#textbox").keypress(function(event) {
+    if (event.which == 13) {
+      if ($("#enter").prop("checked")) {
+        event.preventDefault();
+        $("#send").click();
+      }
+    }
+  });
+ 
+  $("#send").click(function() {
+    var username = "<br><span class='username'>You : </span>"
+    var message = $("#textbox").val();
+    $("#textbox").val("");
+ 
+    var prevMessage = $("#container").html();
+ 
+    // console.log(prevMessage.length);
+    if (prevMessage.length != 0 || prevMessage != "")
+      prevMessage = prevMessage + "";
+ 
+    $("#container").html(prevMessage + username + message);
+ 
+    $("#container").scrollTop($("#container").prop("scrollHeight"));
+ 
+    ai(message);
+ 
+  });
+ 
+ 
+});
+ 
